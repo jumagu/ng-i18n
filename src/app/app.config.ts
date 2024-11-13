3fd97ea65a8c71545d5005fc8d99f3e2bfe03ae3
jumagu <jmgg.gutierrez@gmail.com>
2024-11-13 14:03:18 -0500

    chore: add and configure ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,37 @@
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
 import { routes } from './app.routes';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+    SsrCookieService,
   ],
 };
